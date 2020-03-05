45e2f46d8da6bf8b5947fe6dbbd5ddfa26fa4986
Philip Lee-Chin <phil.leechin@outlook.com>
2020-03-05 02:47:27 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,42 @@
+class CashRegister
+  attr_reader :discount, :total
 
+  def initialize(discount_amount=0)
+    @total = 0
+    @discount = discount_amount
+    @items = []
+  end
+
+  def total=(num)
+    @total = num
+  end
+
+  # def total
+  #   @total
+  # end
+
+  def add_item(title, price)
+    @total += (price * quantity)
+    quantity.times {@item << title}
+    @l_item = (price * quantity)
+  end
+
+  def apply_discount
+    if @discount > 0
+    disc_percentage = (@discount.to_f / 100)
+    @total -= (@total *   disc_percentage)
+    "After the discount, the total comes to $#{@total.to_i}."
+    else
+      "There is no discount to apply."
+  end
+
+  def items
+    @items
+  end
+
+  def void_last_transaction
+    @items.pop
+    @total -= @counter
+  end
+
+end
