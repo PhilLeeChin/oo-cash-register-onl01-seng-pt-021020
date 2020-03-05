71b45042182bf4a5653ba0d26d89074b322dd2d2
Philip Lee-Chin <phil.leechin@outlook.com>
2020-03-05 03:10:31 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,7 +14,7 @@ class CashRegister
   def add_item(title, price)
     quantity = 1
     @total += (price * quantity)
-    #quantity.times {@items << title}
+    quantity.times {|title| @items << title}
     @l_item = (price * quantity)
   end
 
